2a704f470a542d9f816cc386728bedbd9e3ab17f
Ke Chen <ke7@ualberta.ca>
2019-10-30 22:48:43 -0500

    add exercise 1.14

---
+++ Chapter1/Section1.6/exe1.14/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/* program to print a histogram of the frequencies
+ * of different characters in its input
+ * Use ASCII values
+ */
+
+#define MAX 95
+
+int main()
+{
+    int i, j, c;
+    int freqc[MAX];
+    
+    for (i = 0; i < MAX; ++i)
+        freqc[i] = 0;
+    
+    while ( (c = getchar()) != EOF )
+    {
+        if (c >= ' ' && c <= '~')
+        ++freqc[c - ' '];
+    }
+    
+    //print a histogram of the frequencies of different characters
+    printf("The horizontal histogram:\n");
+    for (i = 0; i < MAX; ++i)
+    {
+        printf("|%c|",(char) (i + 32));
+        for (j = 0; j < freqc[i]; ++j)
+        {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+}
