bf1b16e03a9d4986e30a6cd0c77a153f22b846bc
kechen89 <ke7@ualberta.ca>
2020-04-15 18:08:09 -0500

    add exercise 4.2

--- Chapter4/Section4.2/exe4.2/main.c
+++ Chapter4/Section4.2/exe4.2/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <math.h>
 /*
  * Exercise 4.2. Extend atof to handle scientific notation of the form 123.45e-6, where a floating-point number may be followed by e or E and an
  * optionally signed exponent.
@@ -11,6 +12,8 @@ int get_line(char s[], int lim);
 
 double atof_kr(char s[]);
 
+double myatof(char s[]);
+
 int main()
 {
     double sum;
@@ -18,7 +21,7 @@ int main()
     
     sum = 0;
     while (get_line(line, MAXLINE) > 0)
-        printf("\t%g\n", sum += atof_kr(line));
+        printf("\t%g\n", sum += myatof(line));
     return 0;
 }
 
@@ -56,4 +59,50 @@ double atof_kr(char s[])
     }
     
     return sign * val / power;
+}
+
+/* atof_kr: convert string s to double */
+double myatof(char s[])
+{
+    double val, power;
+    int i, sign, exp_sign = 1, n;
+    
+    for (i = 0; isspace(s[i]); i++)     /* skip white space */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] -'0');
+    if (s[i] == '.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++)
+    {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    
+    val /= power;
+    
+    if (s[i] == 'e' || s[i] == 'E')
+    {
+        i++;
+        if (s[i] == '-')
+        {
+            exp_sign = -1;
+            i++;
+        }
+        else if (s[i] == '+')
+        {
+            exp_sign = 1;
+            i++;
+        }
+        
+        for (n = 0; isdigit(s[i]); i++)
+            n = 10 * n + (s[i] - '0');
+        val *= pow(10, exp_sign * n);
+    }
+    
+    return sign * val;
+    
 }
