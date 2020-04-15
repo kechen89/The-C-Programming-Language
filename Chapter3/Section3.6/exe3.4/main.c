777b4fa8fff758b5ebb7dea1d0955ad3ae14db9b
kechen89 <ke7@ualberta.ca>
2020-04-14 20:22:43 -0500

    add exercise 3.4

--- Chapter3/Section3.6/exe3.4/main.c
+++ Chapter3/Section3.6/exe3.4/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*
  * Exercise 3.4. In a two's complement number representation, our version of itoa does not handle the largest negative number,
@@ -10,34 +11,56 @@
 #define MAXLINE 1000
 
 void itoa_kr(int n, char s[]);
+void myitoa(int n, char s[]);
 void reverse(char s[]);
 
 int main()
 {
     int n;
     char s[MAXLINE];
-    
+    printf("Size of int is %lu bytes\n",sizeof(int));
     printf ("Input an integer\n");
     scanf("%d", &n);
-    itoa_kr(n, s);
+    myitoa(n, s);
     
     printf("The converted string is %s\n",s);
     return 0;
 }
 
 /* itoa_kr: convert n to characters in s (The C programming language, Second Edition) */
-/* does not handle the largest negative number in two's complement number machine */
+/* does not handle the largest negative integer number (-2147483648) in two's complement number machine */
 void itoa_kr(int n, char s[])
 {
     int i, sign;
     
     if ((sign = n) < 0)            /* record sign */
-        n = -n;                    /* make n positive */
+        n = -n;                    /* make n positive (the number in memory will overflow if n = -2147483648), -(-2147483648) = -2147483648 */
     i = 0;
     
     do {                           /* generate digits in reverse order */
         s[i++] = n % 10 + '0';     /* get next digit */
     } while ((n /= 10) > 0);         /* delete it */
+
+    
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
+/* myitoa: convert n to characters in s */
+/* can handle the largest negative integer number (-2147483648) in two's complemet number machine */
+void myitoa(int n, char s[])
+{
+    int i, sign;
+    
+    if (n < 0)                     /* record sign */
+        sign = -1;                    /* make n positive */
+    i = 0;
+    
+    do {                           /* generate digits in reverse order */
+        s[i++] = abs(n % 10) + '0';     /* get next digit */
+    } while (abs(n /= 10) > 0);         /* delete it */
 
     
     if (sign < 0)
