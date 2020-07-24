0bc026ae4672af8496d5692da89884aac0e48c65
kechen89 <ke7@ualberta.ca>
2020-07-24 15:57:17 -0500

    add exercise 5.5

--- Chapter5/Section5.5/exe5.5/main.c
+++ Chapter5/Section5.5/exe5.5/main.c
@@ -7,7 +7,11 @@
 */
 
 void myStrncpy(char *s, char *t, int n);
+void myStrncat(char *s, char *t, int n);
+int myStrncmp(char *s, char *t, int n);
+
 void strncpy_C_answer_book(char *s, char *t, int n);
+int strncmp_C_answer_book(char *s, char *t, int n);
 
 int main()
 {
@@ -20,20 +24,68 @@ int main()
     printf("Enter the number of characters to copy from t to s \n");
     scanf("%d", &n);
     
-    myStrncpy(s, t, n);
+    //myStrncpy(s, t, n);
+    //myStrncat(s, t, n);
+    printf("s is larger than n: %d\n", myStrncmp(s, t, n));
+    //printf("s is larger than n: %d\n", strncmp_C_answer_book(s, t, n));
     //strncpy_C_answer_book(s, t, n);
-    printf("Copy at most %d characters of t to s is %s \n", n, s);
+    //printf("Copy at most %d characters of t to s is %s \n", n, s);
     return 0;
 }
 
-/* myStrncpy: copies at most n characters of t to s */
+/*
+ * myStrncpy: copy at most n characters of string t to s,
+ * pad with '\0' if t has fewer than n characters
+ */
 void myStrncpy(char *s, char *t, int n)
 {
     int i = 0;
+    
     while (i < n && (*s++ = *t++) != '\0')
         i++;
     
+    for (; *s != '\0' && i < n; i++)   // if t has fewer than n characters
+        *s++ = '\0';
+    
     return;
+}
+
+/*
+ * myStrncat: concatenate at most n characters of string t to s,
+ * terminate s with '\0'
+ */
+void myStrncat(char *s, char *t, int n)
+{
+    int i = 0;
+    
+    while (*s != '\0')   // find the end of s
+        s++;
+    
+    while (i < n && (*s++ = *t++) != '\0')  // copy t to s
+        i++;
+    
+    if (*s != '\0')
+        *s = '\0';
+}
+
+/*
+ * myStrncmp: compare at most n characters of string s to string t;
+ * return < 0 if s < t, 0 if s == t, or > 0 if s > t.
+ */
+
+int myStrncmp(char *s, char *t, int n)
+{
+    while (n > 0 && (*s == *t) && *s != '\0' && *t != '\0')
+    {
+        s++;
+        t++;
+        n--;
+    }
+        
+    if (n <= 0)
+        return 0;
+    else
+        return *s - *t;
 }
 
 void strncpy_C_answer_book(char *s, char *t, int n)
@@ -42,4 +94,12 @@ void strncpy_C_answer_book(char *s, char *t, int n)
         *s++ = *t++;
     while (n-- > 0)
         *s++ = '\0';
+}
+
+int strncmp_C_answer_book(char *s, char *t, int n)
+{
+    for ( ; *s == *t; s++, t++)
+        if (*s == '\0' || --n <= 0)
+            return 0;
+    return *s - *t;
 }
