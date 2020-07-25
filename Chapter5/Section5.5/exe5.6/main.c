1cf33f37f04348528b4f8b40610c82611858f8ff
kechen89 <ke7@ualberta.ca>
2020-07-24 19:52:33 -0500

    add exercise 5.6 itoa_kr

--- Chapter5/Section5.5/exe5.6/main.c
+++ Chapter5/Section5.5/exe5.6/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 /* Exercise 5.6. Rewrite appropriate programs from earlier chapters and exercises
  * with pointers instead of array indexing. Good possibilities include getline (Chapter 1 and 4),
  * atoi, itoa, and their variants (Chapters 2, 3, and 4), reverse (Chapter 3), and strindex and
@@ -13,18 +14,25 @@ int atoi_ptr(char *s);
 
 int getline_kr(char s[], int lim);
 int atoi_kr(char s[]);
+void itoa_kr(char s[], int n);
+void reverse_kr(char s[]);
 
 int main()
 {
-    int len;
+    int len, n;
     char line[MAXLINE];
     //while ( (len = getline_ptr(line, MAXLINE)) > 0 )
     //    printf("The input line is %s  its length is %d \n", line, len);
     
     char s[MAXLINE];
-    printf("Enter a string of integers \n");
-    scanf("%s", s);
-    printf("Convert to integer %d \n", atoi_ptr(s));
+    //printf("Enter a string of integers \n");
+    //scanf("%s", s);
+    //printf("Convert to integer %d \n", atoi_ptr(s));
+    
+    printf("Enter an integer \n");
+    scanf("%d", &n);
+    itoa_kr(s, n);
+    printf("Convert integer to string %s \n", s);
     
     return 0;
 }
@@ -102,5 +110,41 @@ int atoi_kr(char s[])
     return sign * n;
 }
 
+void itoa_kr(char s[], int n)
+{
+    int sign, i;
+    
+    sign = (n < 0) ? -1 : 1;
+    
+    n = sign * n;
+    
+    i = 0;
+    while (n != 0)
+    {
+        s[i] = n % 10 + '0';
+        n /= 10;
+        i++;
+    }
+ 
+    if (sign == -1)
+    {
+        s[i] = '-';
+        i++;
+    }
+    s[i] = '\0';
+       
+    reverse_kr(s);
+}
 
-
+void reverse_kr(char s[])
+{
+    int i, j;
+    char c;
+    
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
